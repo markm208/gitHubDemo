eac0c45a7fbf5a0844a05fff26aa2e2e46d0e5d9
Mark Mahoney <markm208@gmail.com>
2017-10-04 13:03:33 -0500

    Changed the base to a float.

--- main.cpp
+++ main.cpp
@@ -2,25 +2,25 @@
 
 using namespace std;
 
-void power(int base, int exp);
+void power(float base, int exp);
 
 int main()
 {
-    power(2, 3);
-    power(2, 10);
-    power(2, 20);
+    power(2.5, 3);
+    power(2.5, 10);
+    power(2.5, 20);
 
     return 0;
 }
 //--
-void power(int base, int exp)
+void power(float base, int exp)
 {
-    int result = 1;
+    float result = 1.0;
 
     for(int i = 0;i < exp;i++)
     {
         result = result * base;
     }
 
-    cout<<result<<endl;
+    cout<<fixed<<result<<endl;
 }
